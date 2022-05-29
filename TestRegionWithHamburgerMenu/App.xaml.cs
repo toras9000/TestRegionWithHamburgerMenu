f16f0c553bd7dd6a18062ec92f18331e9aa6fc9e
toras9000 <toras9000@example.com>
2022-05-29 22:17:23 +0900

    プロジェクト ファイルを追加します。

---
+++ TestRegionWithHamburgerMenu/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Prism.Ioc;
+using TestRegionWithHamburgerMenu.Views;
+
+namespace TestRegionWithHamburgerMenu
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+    }
+}
---
+++ TestRegionWithHamburgerMenu/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Prism.Mvvm;
+
+namespace TestRegionWithHamburgerMenu.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
